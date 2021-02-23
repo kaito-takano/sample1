7aac8d23ca759751ead0cee699c9f29a21a6f621
kaito-takano <red.blue.true.akane+GitHub@gmail.com>
2021-02-23 13:05:06 +0900

    三回目

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -10,7 +10,8 @@ class PostController extends Controller
     public function index(Post $post)
     {
         /*return $post->get();*/
-        return view('index')->with(['posts' =>$post->get()]);
+        //return view('index')->with(['posts' =>$post->getByLimit()]);
+        return view('index')->with(['posts' => $post->getPaginateByLimit(5)]);
     }
 }
 ?>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,4 @@
 });*/
 /*Route::get('/posts', 'PostController@index');*/
 Route::get('/', 'PostController@index');
+Route::get('/posts/{post}', 'PostController@show');
