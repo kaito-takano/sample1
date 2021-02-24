33b3e832a3cbe2a04befce92d2788f4e993299e7
kaito-takano <red.blue.true.akane+GitHub@gmail.com>
2021-02-24 11:14:58 +0900

    20210224_一旦

--- app/Post.php
+++ app/Post.php
@@ -6,12 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
-    public function getPaginateByLimit(int $limit_count = 9)
+    protected $fillable = [
+        'title',
+        'body',
+    ];
+    
+    public function getPaginateByLimit(int $limit_count = 30)
     {
         //ascで一から順番、descで大きい順番
         // updated_atで降順に並べたあと、limitで件数制限をかける
         //return $this->orderBy('created_at', 'asc')->limit($limit_count)->get();
         return $this->orderBy('created_at', 'asc')->paginate($limit_count);
     }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,11 @@
 |
 */
 
-/*Route::get('/', function() {
-    return view('index');
-});*/
-/*Route::get('/posts', 'PostController@index');*/
+
 Route::get('/', 'PostController@index');
+Route::get('/posts/create', 'PostController@create');
 Route::get('/posts/{post}', 'PostController@show');
+
+Route::post('/posts', 'PostController@store');
+
+?>
